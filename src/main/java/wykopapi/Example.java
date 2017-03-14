d7ed0e4ffe824b8efa4007eae7b7c526b7b67800
awizisieakat <awizisie.akat@o2.pl>
2017-03-14 16:43:20 +0100

    properties and examples changes

--- src/main/java/wykopapi/Example.java
+++ src/main/java/wykopapi/Example.java
@@ -4,6 +4,7 @@ import wykopapi.examples.SaveVotersAvatars;
 import wykopapi.executor.RequestExecutor;
 import wykopapi.properties.FilePropertiesService;
 import wykopapi.properties.PropertiesService;
+import wykopapi.properties.SimplePropertiesService;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -12,6 +13,7 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) throws IOException {
         PropertiesService propertiesService = new FilePropertiesService("src/main/resources/application.properties");
+        //PropertiesService propertiesService = new SimplePropertiesService.Builder("appkey", "secret").build();
         RequestExecutor executor = new RequestExecutor(propertiesService);
 
         SaveVotersAvatars saveVotersAvatars = new SaveVotersAvatars();
