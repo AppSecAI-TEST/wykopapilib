b771a425744387da6ae5a010e4f8e0dad53d93ed
awizisieakat <awizisie.akat@o2.pl>
2017-03-16 00:33:22 +0100

    add shitstorm example xd

--- src/main/java/wykopapi/examples/AddEntryExample.java
+++ src/main/java/wykopapi/examples/AddEntryExample.java
@@ -1,11 +1,15 @@
 package wykopapi.examples;
 
+import wykopapi.api.dto.EntryOperation;
+import wykopapi.api.dto.Profile;
 import wykopapi.api.executor.RequestExecutor;
 import wykopapi.api.properties.FilePropertiesService;
 import wykopapi.api.properties.PropertiesService;
+import wykopapi.api.request.entries.AddEntryCommentRequest;
 import wykopapi.api.request.entries.AddEntryRequest;
 import wykopapi.api.request.user.LoginRequest;
 
+import java.io.File;
 import java.io.IOException;
 
 public class AddEntryExample {
@@ -15,14 +19,25 @@ public class AddEntryExample {
         RequestExecutor executor = new RequestExecutor(propertiesService);
 
         LoginRequest loginRequest = new LoginRequest.Builder(propertiesService.getAccountKey()).build();
-        String userKey = executor.execute(loginRequest)
-                .orElseThrow(RuntimeException::new)
-                .getUserkey();
+        Profile profile = executor.execute(loginRequest)
+                .orElseThrow(RuntimeException::new);
 
         AddEntryRequest addEntryRequest = new AddEntryRequest
-                .Builder(userKey, "słucham psa jak gra")
+                .Builder(profile.getUserkey(), "słucham psa jak gra")
+                .setEmbedFile(new File("src/main/resources/dogpiano.jpg"))
                 .build();
-        executor.execute(addEntryRequest)
+        EntryOperation entryOperation = executor.execute(addEntryRequest)
+                .ifSuccess(e -> System.out.println(e.getId()))
+                .ifError(System.out::println)
+                .get();
+
+        AddEntryCommentRequest addEntryCommentRequest = new AddEntryCommentRequest
+                .Builder(profile.getUserkey(), entryOperation.getId(),
+                "@" + profile.getLogin() + ": ja też xd")
+                .setEmbedFile(new File("src/main/resources/dogpiano.jpg"))
+                .build();
+
+        executor.execute(addEntryCommentRequest)
                 .ifSuccess(e -> System.out.println(e.getId()))
                 .ifError(System.out::println);
     }
---
+++ src/main/java/wykopapi/examples/AfkShitstorm.java
@@ -0,0 +1,77 @@
+package wykopapi.examples;
+
+import wykopapi.api.dto.Entry;
+import wykopapi.api.dto.EntryOperation;
+import wykopapi.api.dto.Profile;
+import wykopapi.api.executor.RequestExecutor;
+import wykopapi.api.properties.FilePropertiesService;
+import wykopapi.api.properties.PropertiesService;
+import wykopapi.api.request.entries.AddEntryRequest;
+import wykopapi.api.request.entries.GetEntryRequest;
+import wykopapi.api.request.user.LoginRequest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Turn this on and go out for a few hours
+ */
+public class AfkShitstorm {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        PropertiesService propertiesService =
+                new FilePropertiesService("src/main/resources/application.properties");
+        RequestExecutor executor = new RequestExecutor(propertiesService);
+
+        LoginRequest loginRequest = new LoginRequest.Builder(propertiesService.getAccountKey()).build();
+        Profile profile = executor.execute(loginRequest)
+                .orElseThrow(RuntimeException::new);
+
+        String[] texts = {
+                "Kobieta kończy się na 50 kg",
+                "Mężczyzna zaczyna się od 180 cm",
+                "po ile cebula w biedronce? #cebuladeals",
+                "mój kot przeszedł po klawiaturze i dostał sraczki, wołam #programowanie bo wy często używacie klawiatury, może wiecie czemu",
+                "zrobiłbym #rozdajo ale nawet nie wiem co xd",
+                "tylko pedały kupują ajfony i macbooki, prawdziwi mężczyźni mają androida #ios #android",
+                "haha androidy mają tylko biedaki których nie stać na ajfona #ios #android"
+        };
+
+        Random random = new Random();
+        List<Integer> entriesIds = new ArrayList<>();
+
+        while (true) {
+            int minutes = random.nextInt(30) + 30;
+            int text = random.nextInt(texts.length);
+
+            if (entriesIds.size() > 0) {
+                entriesIds.forEach(id -> {
+                    GetEntryRequest getEntryRequest = new GetEntryRequest
+                            .Builder(id).build();
+                    executor.execute(getEntryRequest)
+                            .ifSuccess(entry -> {
+                                System.out.println("Wpis: " + entry.getBody());
+                                System.out.println("Ilość plusów: " + entry.getVoteCount());
+                                System.out.println("Ilość komentarzy: " + entry.getCommentCount());
+                            });
+                });
+            }
+
+            TimeUnit.MINUTES.sleep(minutes);
+
+            AddEntryRequest addEntryRequest = new AddEntryRequest
+                    .Builder(profile.getUserkey(), texts[text])
+                    .build();
+
+            executor.execute(addEntryRequest)
+                    .ifSuccess(e -> {
+                        System.out.println("Added: " + texts[text]);
+                        entriesIds.add(e.getId());
+                    })
+                    .ifError(System.out::println);
+        }
+    }
+}
